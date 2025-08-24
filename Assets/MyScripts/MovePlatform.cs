8093920e49813d5a91a00c69316d71405da5356e
Jceek <guter.seek@gmail.com>
2025-08-24 17:30:05 +0200

    Reduce size by removing the music

---
+++ Assets/MyScripts/MovePlatform.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovePlatform : MonoBehaviour
+{
+    public float mSpeed;
+    public AudioSource moveSound;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+
+}
