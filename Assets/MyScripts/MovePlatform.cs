2d99f61806f156c9f607b4fec8ed7271a79e6c31
Jceek <guter.seek@gmail.com>
2025-08-27 12:05:05 +0200

    Working on basic platform movement

--- Assets/MyScripts/MovePlatform.cs
+++ Assets/MyScripts/MovePlatform.cs
@@ -3,21 +3,35 @@ using UnityEngine.UIElements;
 
 public class MovePlatform : MonoBehaviour
 {
-    public float mSpeed;
+    public float moveSpeed = 1.0f;
     public AudioSource moveSound;
+    private GameObject wayPoint;
+
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
- 
+
+        wayPoint = GameObject.Find("FinishWP");
+       
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
-
+        MovePlatformWayPoint();
     }
-
-
+    private void MovePlatformWayPoint()
+    {
+        if (transform.position != wayPoint.transform.position)
+        {
+            Vector3 moveDirection = (wayPoint.transform.position - transform.position).normalized;
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(0, 0, 0);
+        } 
+    }
 }
